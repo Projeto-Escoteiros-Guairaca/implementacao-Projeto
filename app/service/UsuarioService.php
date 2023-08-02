71b89f2a34397444598f861d64e29534c39bbaa2
Guillermo <guillermotoro61@gmail.com>
2023-08-02 14:52:24 -0300

    Usuario organizado.

--- app/service/UsuarioService.php
+++ app/service/UsuarioService.php
@@ -6,59 +6,63 @@ require_once(__DIR__ . "/../model/Contato.php");
 
 class UsuarioService {
 
-    /* Método para validar os dados do usuário que vem do formulário */
-    public function validarDados(Endereco $endereco, Contato $contato,Usuario $usuario,string $confSenha) {
-        $erros = array();
-        //Validar campos vazios
+    public function validarEndereco(Endereco $endereco) {
+        $ErrorEndereco = array();
+        if(! $endereco->getCep())
+            array_push($ErrorEndereco, "O campo [CEP] é obrigatório.");
+
+        if(! $endereco->getLogradouro())
+            array_push($ErrorEndereco, "O campo [Logradouro] é obrigatório.");
+
+        if(! $endereco->getNumeroEndereco())
+            array_push($ErrorEndereco, "O campo [Número] é obrigatório.");
+    
+        if(! $endereco->getBairro())
+            array_push($ErrorEndereco, "O campo [Bairro] é obrigatório.");
+
+        if(! $endereco->getCidade())
+            array_push($ErrorEndereco, "O campo [Cidade] é obrigatório.");
+
+        if(! $endereco->getPais())
+            array_push($ErrorEndereco, "O campo [País] é obrigatório.");
+    return $ErrorEndereco;
+    }
+    public function validarContato( Contato $contato) {
+        $errorContato = array();
+        if(! $contato->getCelular())
+            array_push($errorContato, "O campo [Celular] é obrigatório.");
+
+        if(! $contato->getEmail())
+            array_push($errorContato, "O campo [E-mail] é obrigatório.");
+    return $errorContato;
+    }
+    public function validarUsuario(Usuario $usuario,string $confSenha) {
+        $errorUsuario = array();
+
         if(! $usuario->getNome())
-            array_push($erros, "O campo [Nome] é obrigatório.");
+            array_push($errorUsuario, "O campo [Nome] é obrigatório.");
 
         if(! $usuario->getLogin())
-            array_push($erros, "O campo [Login] é obrigatório.");
+            array_push($errorUsuario, "O campo [Login] é obrigatório.");
 
         if(! $usuario->getSenha())
-            array_push($erros, "O campo [Senha] é obrigatório.");
+            array_push($errorUsuario, "O campo [Senha] é obrigatório.");
 
         if(! $confSenha)
-            array_push($erros, "O campo [Confirmação da senha] é obrigatório.");
+            array_push($errorUsuario, "O campo [Confirmação da senha] é obrigatório.");
         
         if(! $usuario->getCpf())
-            array_push($erros, "O campo [CPF] é obrigatório.");
+            array_push($errorUsuario, "O campo [CPF] é obrigatório.");
 
         if(! $usuario->getPapeis())
-            array_push($erros, "Selecione pelo menos um papel no campo [Papéis do Usuário].");
+            array_push($errorUsuario, "Selecione pelo menos um papel no campo [Papéis do Usuário].");
 
         //Validar se a senha é igual a contra senha
         if($usuario->getSenha() != $confSenha)
-            array_push($erros, "O campo [Senha] e [Confirmação da senha] devem ser iguais.");
-
-        if(! $endereco->getCep())
-            array_push($erros, "O campo [CEP] é obrigatório.");
-
-        if(! $endereco->getLogradouro())
-            array_push($erros, "O campo [Logradouro] é obrigatório.");
-
-        if(! $endereco->getNumeroEndereco())
-            array_push($erros, "O campo [Número] é obrigatório.");
-        
-        if(! $endereco->getBairro())
-            array_push($erros, "O campo [Bairro] é obrigatório.");
-
-        if(! $endereco->getCidade())
-            array_push($erros, "O campo [Cidade] é obrigatório.");
-
-        if(! $endereco->getPais())
-            array_push($erros, "O campo [País] é obrigatório.");
-
-        if(! $contato->getCelular())
-            array_push($erros, "O campo [Celular] é obrigatório.");
-
-        if(! $contato->getEmail())
-            array_push($erros, "O campo [E-mail] é obrigatório.");
-        return $erros;
+            array_push($errorUsuario, "O campo [Senha] e [Confirmação da senha] devem ser iguais.");
+            
+    return $errorUsuario;
     }
-
-
     public function insertUsu(Usuario $usuario){
         $usuarioDao = new UsuarioDAO();
         $usuarioDao->insert($usuario);
