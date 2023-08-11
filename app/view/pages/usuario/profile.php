dc0b6537721af22c55db79eb2b591a36cc22c987
Ricardo Augusto <ricardo.troche.tds@gmail.com>
2023-08-11 03:31:09 -0300

    blublu3

--- app/view/pages/usuario/profile.php
+++ app/view/pages/usuario/profile.php
@@ -7,69 +7,69 @@ require_once(__DIR__ . "/../../../model/enum/UsuarioPapel.php");
 <link rel="stylesheet" type="text/css" href="../view/styles/profile.css?v=<?php echo time(); ?>">
 
 <div class="d_total">
-    <a class="btn btn-primary" href='<?= HOME_PAGE ?>'>Voltar</a>
+    <a class="btn_voltar_prof btn btn-primary" href='<?= HOME_PAGE ?>'>Voltar</a>
                 
     <h1> Dados do Usuário </h1>
-    <button> <a href="<?= BASEURL ?>
+    <button id="btn_altercao"> <a href="<?= BASEURL ?>
         /controller/UsuarioController.php?action=edit&id=<?=$dados['usuario']->getId()?>">
-        Quer alterar teus Dados? </a>  </button>
+        Quer alterar seus Dados? </a>  </button>
         <br>
   <div class="row justify-content-center">
     <div id="usuario" class="col-3 m-4">
-    <h2>Usuario</h2>
+    <h2>Usuário</h2>
 
         Nome:
         <br>
         <p class ="p_dados"> <?= $dados['usuario']->getNome(); ?> <p>
         <br>
-        Nome do Login:
+        Nome do Usuário:
         <br>
         <p class ="p_dados"> <?= $dados['usuario']->getLogin(); ?> <p>
         <br>
-        Teu papel no sistema:
+        Classificacão:
         <br>
         <p class ="p_dados"> <?= $dados['usuario']->getPapeisStr(); ?><p>
         <br>
-        Tua senha: 
+        Senha: 
         <br>
         <p class ="p_dados"> <?= $dados['usuario']->getSenha(); ?> <p>
         <br>
     </div>
 
     <div  id="endereco" class="col-3 m-4">
-    <h2>Endereco </h2>
+    <h2>Endereço </h2>
     
-        Teu CEP: <br>
-        <?= $dados['usuario']->getEndereco()->getCep(); ?> 
+        CEP: <br>
+        <p class ="p_dados"><?= $dados['usuario']->getEndereco()->getCep(); ?> </p>
         <br>
         Logradouro: <br>
-        <?=$dados['usuario']->getEndereco()->getLogradouro(); ?>
+        <p class ="p_dados"> <?=$dados['usuario']->getEndereco()->getLogradouro(); ?> </p>
         <br>
-        Teu numero de endereço:<br>
-        <?= $dados['usuario']->getEndereco()->getNumeroEndereco(); ?>
+        Número de endereço:<br>
+        <p class ="p_dados"> <?= $dados['usuario']->getEndereco()->getNumeroEndereco(); ?> </p>
         <br>
         Bairro:<br>
-        <?= $dados['usuario']->getEndereco()->getBairro(); ?>
+        <p class ="p_dados"> <?= $dados['usuario']->getEndereco()->getBairro(); ?> </p>
         <br>
-        cidade:<br>
-        <?= $dados['usuario']->getEndereco()->getCidade(); ?> 
+        Cidade:<br>
+        <p class ="p_dados"> <?= $dados['usuario']->getEndereco()->getCidade(); ?> </p>
         <br>
-         Pais:<br>
-        <?= $dados['usuario']->getEndereco()->getPais(); ?>
+        País:<br>
+        <p class ="p_dados"> <?= $dados['usuario']->getEndereco()->getPais(); ?> </p>
         <br>
     </div>
     
     <div id="contato" class="col-3 m-4">
     <h2>Contato </h2>
         
-        Telefone de casa: <br>
-        <?= $dados['usuario']->getContato()->getTelefone(); ?> 
+        Telefone residencial: <br>
+        <p class ="p_dados"> <?= $dados['usuario']->getContato()->getTelefone(); ?> </p>
         <br>
         Celular:<br>
-        <?= $dados['usuario']->getContato()->getCelular(); ?>
+        <p class ="p_dados">  <?= $dados['usuario']->getContato()->getCelular(); ?> </p>
         <br>
         Email: <br>
-        <?= $dados['usuario']->getContato()->getEmail(); ?>
+        <p class ="p_dados"> <?= $dados['usuario']->getContato()->getEmail(); ?> </p>
         <br>
     </div>
   </div> 
