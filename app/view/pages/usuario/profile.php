cd6bf78a61b949cfc46ee1da2487042a2da27537
Ricardo Augusto <ricardo.troche.tds@gmail.com>
2023-08-13 03:37:42 -0300

    blublu10

--- app/view/pages/usuario/profile.php
+++ app/view/pages/usuario/profile.php
@@ -6,6 +6,7 @@ require_once(__DIR__ . "/../../../model/enum/UsuarioPapel.php");
 <?php require_once(__DIR__ . "/../../include/menu.php");?>
 <link rel="stylesheet" type="text/css" href="../view/styles/profile.css?v=<?php echo time(); ?>">
 
+
 <div class="d_total">
     <a id="btn_voltar_profile" class=" btn " href='<?= HOME_PAGE ?>'>Voltar</a>
                 
@@ -14,9 +15,11 @@ require_once(__DIR__ . "/../../../model/enum/UsuarioPapel.php");
         /controller/UsuarioController.php?action=edit&id=<?=$dados['usuario']->getId()?>">
         Quer alterar seus Dados? </a>  </button>
         <br>
-  <div class="row justify-content-center">
+  <div class="row justify-content-center" id="apresentacoes">
 
-  <button> Usuario </button>
+  <button class="b_apresentacoes btn btn-primary"> <i class="bi bi-person-fill"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+</svg>Usuario </button>
   <dialog>
     <div id="usuario" class="col-3 m-4">
     <h2>Usuário</h2>
@@ -40,7 +43,7 @@ require_once(__DIR__ . "/../../../model/enum/UsuarioPapel.php");
     </div>
   </dialog>
 
-  <button> Endereco </button>
+  <button class="b_apresentacoes btn btn-primary">  <i class="fas fa-home"></i> Endereco </button>
   <dialog>
     <div  id="endereco" class="col-3 m-4">
     <h2>Endereço </h2>
@@ -66,7 +69,9 @@ require_once(__DIR__ . "/../../../model/enum/UsuarioPapel.php");
     </div>
   </dialog>
 
-  <button> Contato </button>
+  <button class="b_apresentacoes btn btn-primary"><i class= "bi-telephone-fill"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+</svg>Contato </button>
   <dialog>
     <div id="contato" class="col-3 m-4">
     <h2>Contato </h2>
