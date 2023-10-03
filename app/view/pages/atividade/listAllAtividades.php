fc826e4c1bca7b5e867e4de99ab242fe11ad9097
Ricardo Augusto <ricardo.troche.tds@gmail.com>
2023-10-03 02:42:32 -0300

    blublu

--- app/view/pages/atividade/listAllAtividades.php
+++ app/view/pages/atividade/listAllAtividades.php
@@ -24,7 +24,9 @@ require_once(__DIR__ . "/../alcateia/selectAlcateia.php");
         <div class="row">
             <div class="col-12">
                 <?php foreach($dados["lista"] as $ativ):?>
-                    <div class="card my-2 mx-2" style="width: 18rem;">
+                    <div class="card my-2 mx-2" style="width: 18rem;"> 
+                    <a class= "principais_btn_cards_atv" href="<?= BASEURL?>/controller/AtividadeController.php?action=edit&id=<?php echo $ativ->getIdAtividade()?>"><i class="bi bi-pencil"></i></a>
+                    <a class= "principais_btn_cards_atv" href="<?= BASEURL?>/controller/AtividadeController.php?action=delete&id=<?php echo $ativ->getIdAtividade()?>"> <i class="bi bi-trash3"></i> </a>
                         <img style="height: 225px;" class="card-img-top" src="<?= BASEURL?><?php echo $ativ->getImagem();?>" alt="INSERE IMAGEM AQUI LOL">
                         <div class="card-body">
                             <h5 class="titulos_atv"><?php echo $ativ->getNomeAtividade();?></h5>
@@ -32,10 +34,8 @@ require_once(__DIR__ . "/../alcateia/selectAlcateia.php");
                             <p class="card-text">
                             <?php echo $ativ->getDescricao();?></p>
                             <a class= "btn_cards_atv" href="<?= BASEURL?>/controller/TarefaController.php?action=list&idAtividade=<?php echo $ativ->getIdAtividade()?>">Mostrar Tarefas</a>
-                            <a class= "btn_cards_atv" href="<?= BASEURL?>/controller/AtividadeController.php?action=edit&id=<?php echo $ativ->getIdAtividade()?>">alterar</a>
+                           
                             <a class= "btn_cards_atv" href="<?= BASEURL?>/controller/TarefaController.php?action=createTarefaAtiv&idAtividade=<?php echo $ativ->getIdAtividade()?>">Criar Tarefas</a>
-                            <a class= "btn_cards_atv" href="<?= BASEURL?>/controller/AtividadeController.php?action=delete&id=<?php echo $ativ->getIdAtividade()?>"
-                            > deletar </a>
                         </div>
                     </div>
                 <?php endforeach;?>
