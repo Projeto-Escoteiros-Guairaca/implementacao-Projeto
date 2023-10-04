0856d55cf1ec42fb1a1380679af8d8568a6bf741
Ricardo Augusto <ricardo.troche.tds@gmail.com>
2023-10-04 06:06:18 -0300

    blublu

--- app/view/pages/atividade/listAllAtividades.php
+++ app/view/pages/atividade/listAllAtividades.php
@@ -25,8 +25,8 @@ require_once(__DIR__ . "/../alcateia/selectAlcateia.php");
             <div class="col-12">
                 <?php foreach($dados["lista"] as $ativ):?>
                     <div  class="card my-2 mx-2" style="width: 18rem;"> 
-                    <a id= "editar_atv" class= "principais_btn_cards_atv" href="<?= BASEURL?>/controller/AtividadeController.php?action=edit&id=<?php echo $ativ->getIdAtividade()?>"><i class="bi bi-pencil"></i></a>
-                    <a id= "deletar_atv"class= "principais_btn_cards_atv" href="<?= BASEURL?>/controller/AtividadeController.php?action=delete&id=<?php echo $ativ->getIdAtividade()?>"> <i class="bi bi-trash3"></i> </a>
+                    <a id= "editar_atv" class= "principais_btn_cards_atv" href="<?= BASEURL?>/controller/AtividadeController.php?action=edit&id=<?php echo $ativ->getIdAtividade()?>"><i class="bi bi-pencil"><span id= p_editar_atv >Editar</span></i></a>
+                    <a id= "deletar_atv"class= "principais_btn_cards_atv" href="<?= BASEURL?>/controller/AtividadeController.php?action=delete&id=<?php echo $ativ->getIdAtividade()?>"> <i class="bi bi-trash3"><span id= p_deletar_atv >Deletar</span></i> </a>
                         <img style="height: 225px;" class="card-img-top" src="<?= BASEURL?><?php echo $ativ->getImagem();?>" alt="INSERE IMAGEM AQUI LOL">
                         <div class="card-body">
                             <h5 class="titulos_atv"><?php echo $ativ->getNomeAtividade();?></h5>
