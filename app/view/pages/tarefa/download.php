8e54bf05efbadb468d1389ccc1965ef9c929baaa
Guillermo <guillermotoro61@gmail.com>
2023-11-27 00:39:38 -0300

    funcionando

--- app/view/pages/tarefa/download.php
+++ app/view/pages/tarefa/download.php
@@ -1,7 +1,17 @@
 <?php
 if (isset($_GET['file'])) {
-    $filePath = $_GET['file'];
+    $string = '\"';
+    $string = str_replace(array('"'), "", $string);
+
+    $string2 ="view\pages\'tarefa\'";
+    $string2 = str_replace(array("'"), "", $string2);
+
+    $filePath =  __DIR__ . $_GET['file'];
+    $filePath = str_replace(array('..'), '',$filePath);
+    $filePath = str_replace(array('/'), $string,$filePath);
+    $filePath = str_replace(array($string2), "",$filePath);
     
+    var_dump($filePath); 
     if (file_exists($filePath)) {
         header('Content-Type: application/octet-stream');
         header('Content-Disposition: attachment; filename="' . basename($filePath) . '"');
